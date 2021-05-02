35ac3a8bf2933e03207afe6e1f00c07b91459cb7
mustaphazeroual04 <mustaphazeroual01@gmail.com>
2021-05-02 18:06:44 +0000

    add signin and signup components

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,14 @@
 import React from 'react';
 import AppMenu from './components/AppBar';
+import SignIn from './components/auth/Login';
+import SignUp from './components/auth/Signup';
 
 const App = () => {
     return (
         <div>
             <AppMenu />
+            <SignIn />
+            <SignUp />
         </div>
     )
 }
