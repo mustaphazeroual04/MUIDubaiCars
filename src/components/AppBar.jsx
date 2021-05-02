492e5e19a8304c24d052de5e6827bf2008b67cc7
mustaphazeroual04 <mustaphazeroual01@gmail.com>
2021-05-02 17:19:52 +0000

    finishing Appbar

--- src/components/AppBar.jsx
+++ src/components/AppBar.jsx
@@ -3,8 +3,10 @@ import { createStyles, makeStyles, Theme } from '@material-ui/core/styles';
 import {
     AppBar, Box, 
     Toolbar, Typography,
-    CardMedia, IconButton
+    CardMedia, IconButton,
+    Icon
 } from '@material-ui/core';
+import dubiCarsLogo from "../images/logo.svg";
 
 
 const useStyles = makeStyles((theme) =>
@@ -20,27 +22,38 @@ const useStyles = makeStyles((theme) =>
     },
     menuItemContainer : {
         display : 'flex',
-        backgroundColor: "lightblue",
+        //backgroundColor: "lightblue",
         height : "100%",
-        margin : '0 5px',
+        margin : '0 10px',
         alignItems : "center",
         position : 'relative',
+        color : 'rgb(40, 57, 73)',
         '&:before' : {
             content : "''",
-            width: '100%',
-            border : '2px solid darkcyan',
+            display : 'none',
+            width: '97%',
+            border : '3px solid darkcyan',
             position : "absolute",
             bottom : "-2px",
             left : 0
 
+        },
+        '&:hover' : {
+            color : "#f14b6a",
+            
+            '&:before' : {
+                borderColor : '#f14b6a',
+                display : 'flex',
+            }   
         }
         
     },
     menuItem : {
         //display : "inline-block",
-        color : 'rgb(40, 57, 73)',
-        fontWeight : "bold",
-        fontSize : '1.5rem',
+        color : 'inherit',
+        fontWeight : 100,
+        padding : '0 10px',
+        fontSize : '1.1rem',
         
         
     },
@@ -49,18 +62,46 @@ const useStyles = makeStyles((theme) =>
     },
     toolBar : {
         //height: "100%",
-        //display : 'flex',
-        height: "70px",
+        display : 'flex',
+        justifyContent : "space-between",
+        //flexWrap : 'nowrap',
+        height: "60px",
        // backgroundColor : "red",
-        padding : 0,
+        padding : '0 3rem',
         margin : 0,
         
 
     },
     menuLogo : {
-        marginRight: theme.spacing(10),
+        marginRight: theme.spacing(2),
 
         
+    },
+    imageIcon: {
+        height: "90%",
+        width: '90%'
+    },
+    iconRoot: {
+        textAlign: 'center',
+        //backgroundColor: 'lightblue',
+        width : '4rem',
+        height : '3rem',
+        margin : '5px 0px 5px 0px'
+    },
+    sellButton : {
+        backgroundColor : "#f14b6a",
+        padding : '4px 15px',
+        borderRadius : 20,
+        '&:hover' : {
+            background : 'linear-gradient(90deg, rgb(241,75,106), rgb(150,51,69))',
+            //backgroundColor : "green" 
+        }
+    },
+    
+    itemsContainer : {
+        display : 'flex',
+        flexDirection : 'row',
+        height : '100%'
     }
 
   }),
@@ -73,39 +114,47 @@ export default function AppMenu() {
     <div className={classes.root}>
       <AppBar className={classes.appBar} position="static">
         <Toolbar className={classes.toolBar} >
-        < IconButton edge="start" className={classes.menuLogo} color="inherit" aria-label="menu">
-                <CardMedia
-                    component="img"
-                    alt="DubiCars logo"
-                    
-                    image="../images/logo.svg"
-                    title="DubiCars title"
-                    width="40px"
-                />
-        </IconButton>
+        <Box className={classes.itemsContainer}  >
+            < IconButton edge="start" className={classes.menuLogo} color="inherit" aria-label="menu">
+                <Icon classes={{root: classes.iconRoot}}>
+                    <img className={classes.imageIcon} src={dubiCarsLogo}/>
+                </Icon>
+            </IconButton>
           
-          <Box className={classes.menuItemContainer}>
-
-          
-            <Typography variant="h6" className={classes.menuItem}>
-                Car News
-            </Typography>
+            <Box className={classes.menuItemContainer}>
+                <Typography variant="h6" className={classes.menuItem}>
+                    Car News
+                </Typography>
+            </Box>
+            <Box className={classes.menuItemContainer}>
+                <Typography variant="h6" className={classes.menuItem}>
+                    Export
+                </Typography>
+            </Box>
+            <Box className={classes.menuItemContainer}>
+                <Typography variant="h6" className={classes.menuItem}>
+                    Value My Car
+                </Typography>
+            </Box>
+            <Box className={classes.menuItemContainer}>
+                <Typography variant="h6" className={classes.menuItem}>
+                    About Us
+                </Typography>
+            </Box>
+            <Box className={classes.menuItemContainer}>
+                <Typography variant="h6" className={classes.menuItem}>
+                    LOGIN
+                </Typography>
+            </Box>
+        </Box>
+        
+        <Box>
+            <Box className={classes.sellButton} >
+              <Typography>
+                  SELL MY CAR
+              </Typography>
           </Box>
-          <Box className={classes.menuItemContainer}>
-            <Typography variant="h6" className={classes.menuItem}>
-                Export
-            </Typography>
-          </Box>
-          <Box className={classes.menuItemContainer}>
-            <Typography variant="h6" className={classes.menuItem}>
-                Value My Car
-            </Typography>
-          </Box>
-          <Box className={classes.menuItemContainer}>
-            <Typography variant="h6" className={classes.menuItem}>
-                About Us
-            </Typography>
-          </Box>
+        </Box>
           
           
         </Toolbar>
